f67ca5898c97251706381e9623349a2d500999bb
ChadMcintire <chadmcintirecloud@gmail.com>
2017-10-27 16:38:00 -0600

    Made changes to background

--- app/src/main/java/com/example/chad/smstrialapp/MainActivity.java
+++ app/src/main/java/com/example/chad/smstrialapp/MainActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
     }
           public void CreateText(View view){
         Intent intent = new Intent(this, Send_text.class);
-        startActivity(intent); 
+        startActivity(intent);
         }
 }
