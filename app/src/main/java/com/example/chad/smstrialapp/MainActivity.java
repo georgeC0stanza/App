92ad4c4e1ded74f4458d1120849d47a594b8c900
ChadMcintire <chadmcintirecloud@gmail.com>
2017-10-27 14:35:36 -0600

    Initial commit

---
+++ app/src/main/java/com/example/chad/smstrialapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.chad.smstrialapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+          public void CreateText(View view){
+        Intent intent = new Intent(this, Send_text.class);
+        startActivity(intent); 
+        }
+}
