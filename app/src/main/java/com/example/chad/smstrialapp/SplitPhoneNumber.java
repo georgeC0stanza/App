bd6bdb0ea45c9807fbe609a7f34d2416cdc41584
georgeC0stanza <32494929+georgeC0stanza@users.noreply.github.com>
2017-11-03 16:43:28 -0600

    new class to split up phonenumbers

---
+++ app/src/main/java/com/example/chad/smstrialapp/SplitPhoneNumber.java
@@ -0,0 +1,10 @@
+package com.example.chad.smstrialapp;
+
+
+public class SplitPhoneNumber {
+    public String[] split(String phoneNumber){
+        String[] stringArray = phoneNumber.split(",");
+        return stringArray;
+    }
+}
+
