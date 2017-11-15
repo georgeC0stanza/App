09ca489cb127cc2a4656104022b6827d08140619
georgeC0stanza <32494929+georgeC0stanza@users.noreply.github.com>
2017-11-15 14:33:19 -0700

    clean up send sms

--- app/src/main/java/com/example/chad/smstrialapp/Send_text.java
+++ app/src/main/java/com/example/chad/smstrialapp/Send_text.java
@@ -2,45 +2,21 @@ package com.example.chad.smstrialapp;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.PendingIntent;
-import android.content.Intent;
+//import android.app.PendingIntent;
+//import android.content.Intent;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
-
 
 public class Send_text extends Activity {
 
     Button btnSendSMS;
     EditText txtPhoneNo;
     EditText txtMessage;
-
-    private void sendSMS(String  phoneNumber, String  message) {
-        PendingIntent pi = PendingIntent.getActivity(this, 0,
-                new Intent(this, Send_text.class), 0);
-
-        // split up phone numbers
-        String[] stringArray = SplitPhoneNumber.split(phoneNumber);
-
-        // send text messages
-        SmsManager sms = SmsManager.getDefault();
-
-        // break up long messages
-        ArrayList<String> parts = sms.divideMessage(message);
-
-        for (int i = 0; i < stringArray.length; i++) {
-        //    sms.sendTextMessage(stringArray[i], null, message, null, null);
-            sms.sendMultipartTextMessage(stringArray[i], null, parts, null, null);
-
-        }
-        Toast.makeText(getApplicationContext(), "Message sent!",
-                Toast.LENGTH_SHORT).show();
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -79,4 +55,27 @@ public class Send_text extends Activity {
             }
         });
     }
+
+    private void sendSMS(String  phoneNumber, String  message) {
+//        PendingIntent pi = PendingIntent.getActivity(this, 0,
+//                new Intent(this, Send_text.class), 0);
+
+        // split up phone numbers
+        String[] stringArray = SplitPhoneNumber.split(phoneNumber);
+
+        // send text messages
+        SmsManager sms = SmsManager.getDefault();
+
+        // break up long messages
+        ArrayList<String> parts = sms.divideMessage(message);
+
+        for (int i = 0; i < stringArray.length; i++) {
+//            sms.sendTextMessage(stringArray[i], null, message, null, null);
+            sms.sendMultipartTextMessage(stringArray[i], null, parts, null, null);
+        }
+        Toast.makeText(getApplicationContext(), "Message sent!",
+                Toast.LENGTH_SHORT).show();
+    }
+
+
 }
