a2704ad50c806bf77886785531315b05b69b184d
taftkhris <chr13026@byui.edu>
2017-11-20 16:16:31 -0700

    Start of the parsing function implemente number 2

---
+++ app/src/main/java/com/example/chad/smstrialapp/PopulateTemplate.java
@@ -0,0 +1,19 @@
+package com.example.chad.smstrialapp;
+
+/**
+ * Created by Taft on 11/20/2017.
+ */
+
+public class PopulateTemplate {
+    public String pTemplate(String message, String name, String start, String date){
+        GoogleCalApi gc = new GoogleCalApi();
+
+
+        message = message.replace("<N>", name);
+        message = message.replace("<T>", start);
+        message = message.replace("<D>", date);
+
+        return message;
+    }
+}
+
